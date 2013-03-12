6fad3759a0989c8cf317eae3b33b8d6addfade92
Alex Manelis <amanelis@gmail.com>
2013-03-12 16:34:06 -0700

    Removed old app project

--- app/config/initializers/baboon.rb
+++
@@ -1,9 +0,0 @@
-Baboon.configure do |config|
-  config.application = 'console'
-  config.repository  = 'git@github.com:128lines/console.fm.git'
-  config.deploy_path = '/home/rails/console.fm/'
-  config.deploy_user = :rails
-  config.branch      = :master
-  config.rails_env   = :production
-  config.servers     = ['10.0.0.1', '10.0.0.1']
-end
